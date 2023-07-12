7ac24fad90bd74a99a090644f9f9472ef43ee764
ksanchez99 <killersanchez43@gmail.com>
2023-07-12 03:46:23 -0500

    Added basic HTML file

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+<script>
+        function My_First_Function() {
+        var String = "Kiss me, I'm Irish!";
+        var result = String.fontcolor("green");
+        document.getElementById("Irish").innerHTML = My_First_Functionresul;
+        {"}"}
+    </script>
